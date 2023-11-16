7cdd3f77de42d339667d9f7e3ee64efa98c014aa
Boniface <sollomon35@gmail.com>
2023-11-16 16:24:45 +0200

    refactor

--- src/ui/components/Main/Transactions/Transaction/Metadata/AccessKey/AccessKey.tsx
+++ src/ui/components/Main/Transactions/Transaction/Metadata/AccessKey/AccessKey.tsx
@@ -1,6 +1,27 @@
 import { InputGroup } from '../../../../../general/InputGroup/InputGroup.tsx';
+import { useEffect } from 'react';
+import { KeyPair } from 'near-api-js';
+import { useWatch } from 'react-hook-form';
 
-export const AccessKey = ({ register, signerType }: any) => {
+const getPublicKey = (privateKey: string) => {
+  try {
+    return KeyPair.fromString(privateKey).getPublicKey().toString();
+  } catch (e) {
+    return e;
+  }
+};
+export const AccessKey = ({ form }: any) => {
+  const { register, control, setValue } = form;
+
+  const signerType = useWatch({
+    control,
+    name: 'signerKey',
+  });
+
+  useEffect(() => {
+    setValue('signerKey.publicKey', getPublicKey(signerType?.privateKey));
+  }, [signerType?.privateKey]);
+
   return (
     <fieldset style={{ borderRadius: 8 }}>
       <legend>Access key</legend>
--- src/ui/components/Main/Transactions/Transaction/Metadata/Metadata.tsx
+++ src/ui/components/Main/Transactions/Transaction/Metadata/Metadata.tsx
@@ -1,36 +1,16 @@
 import { InputGroup } from '../../../../general/InputGroup/InputGroup.tsx';
-import { useWatch } from 'react-hook-form';
-import { KeyPair } from 'near-api-js';
-import { useEffect } from 'react';
 import { AccessKey } from './AccessKey/AccessKey.tsx';
 
-const getPublicKey = (privateKey: string) => {
-  try {
-    return KeyPair.fromString(privateKey).getPublicKey().toString();
-  } catch (e) {
-    return e;
-  }
-};
-
 export const Metadata = ({ form }: any) => {
-  const { register, getValues, control, setValue } = form;
+  const { register, getValues } = form;
   const name = getValues().name;
-
-  const signerType = useWatch({
-    control,
-    name: 'signerKey',
-  });
-
-  useEffect(() => {
-    setValue('signerKey.publicKey', getPublicKey(signerType?.privateKey));
-  }, [signerType?.privateKey]);
 
   return (
     <div>
       <h2>{name}</h2>
       <h3>From</h3>
       <InputGroup register={register} name="signer.accountId" label="Signer Id" />
-      <AccessKey register={register} signerType={signerType} />
+      <AccessKey form={form} />
     </div>
   );
 };
