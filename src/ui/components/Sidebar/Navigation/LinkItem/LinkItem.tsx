b691a579b84673bb07bb4882bcdc62c34921580c
Boniface <sollomon35@gmail.com>
2023-12-26 11:52:27 +0200

    refactor

--- src/ui/components/Sidebar/Navigation/LinkItem/LinkItem.tsx
+++ src/ui/components/Sidebar/Navigation/LinkItem/LinkItem.tsx
@@ -4,7 +4,7 @@ import cn from './LinkItem.module.css';
 export const LinkItem = ({ name, src, to }: any) => {
   return (
     <Link className={cn.container} to={to}>
-      <img className={cn.img} alt="#" src={`/${src}.svg`} />
+      <img className={cn.img} alt="#" src={src} />
       <h4 className={cn.title}>{name}</h4>
     </Link>
   );
--- src/ui/components/Sidebar/Navigation/Navigation.tsx
+++ src/ui/components/Sidebar/Navigation/Navigation.tsx
@@ -4,11 +4,11 @@ import { LinkItem } from './LinkItem/LinkItem.tsx';
 export const Navigation = () => {
   return (
     <div className={cn.navigation}>
-      <LinkItem name="Transactions" src="transaction" to="/transactions" />
-      <LinkItem name="Vault" src="vault" to="/vault" />
-      <LinkItem name="Environment" src="environment" to="/environment" />
-      <LinkItem name="Spaces" src="spaces" to="/spaces" />
-      <LinkItem name="Networks" src="networks" to="/networks" />
+      <LinkItem name="Transactions" src="/transaction.svg" to="/transactions" />
+      <LinkItem name="Vault" src="/vault.svg" to="/vault" />
+      <LinkItem name="Environment" src="/environment.svg" to="/environment" />
+      <LinkItem name="Spaces" src="/spaces.svg" to="/spaces" />
+      <LinkItem name="Networks" src="/networks.svg" to="/networks" />
     </div>
   );
 };
