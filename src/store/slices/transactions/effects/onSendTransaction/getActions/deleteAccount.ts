665ebc8b82dcc285a597334a46fea296f2b2aeb1
Boniface <sollomon35@gmail.com>
2024-01-16 13:39:04 +0200

    add action

---
+++ src/store/slices/transactions/effects/onSendTransaction/getActions/deleteAccount.ts
@@ -0,0 +1,6 @@
+import { transactions, utils } from 'near-api-js';
+
+export const deleteAccount= (action: any) => {
+    console.log(action)
+    // return transactions.deleteAccount(action.beneficiaryId);
+};
