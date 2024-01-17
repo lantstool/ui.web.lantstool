e2b866fecf5adfc99a6486e6ce6087925b719e40
Boniface <sollomon35@gmail.com>
2024-01-17 18:10:53 +0200

    rework ImportKey

--- src/ui/components/Main/Vault/Account/ImportKey/ImportKey.tsx
+++ src/ui/components/Main/Vault/Account/ImportKey/ImportKey.tsx
@@ -1,23 +1,12 @@
-import { useState, useEffect } from 'react';
-import { useStoreAction, useStoreEffect, useStoreState } from '../../../../../../react-vault';
-import { SignatureType } from './SignatureType/SignatureType.tsx';
-import { ImportType } from './ImportType/ImportType.tsx';
-import { SeedPhrase } from './SeedPhrase/SeedPhrase.tsx';
-import { PrivateKey } from './PrivateKey/PrivateKey.tsx';
+import { useState } from 'react';
+import { useStoreAction } from '../../../../../../react-vault';
 import addIcon from '../../../../../../assets/addIcon.svg';
 import { Button } from '../../../general/Button/Button.tsx';
-import { onSetKeyList } from './onSetKeyList.ts';
+import { ImportKeyModal } from './ImportKeyModal/ImportKeyModal.tsx';
 
 export const ImportKey = ({ accountId }: any) => {
   const [isOpen, setOpen]: any = useState(false);
-  const [keyList, setKeyList] = useState([]);
-  const modalStep = useStoreState((store: any) => store.vault.route);
   const navigate = useStoreAction((action: any) => action.vault.navigate);
-  const getAccessKeyList = useStoreEffect((store: any) => store.getAccessKeyList);
-
-  useEffect(() => {
-    onSetKeyList(getAccessKeyList, accountId, setKeyList);
-  }, [accountId]);
 
   const openModal = () => {
     setOpen(true);
@@ -31,28 +20,12 @@ export const ImportKey = ({ accountId }: any) => {
   return (
     <>
       <Button text="Import key" onClick={openModal} type="submit" src={addIcon} />
-      {modalStep === 'signatureType' && (
-        <SignatureType closeModal={closeModal} navigate={navigate} isOpen={isOpen} />
-      )}
-      {modalStep === 'importType' && (
-        <ImportType closeModal={closeModal} navigate={navigate} isOpen={isOpen} />
-      )}
-      {modalStep === 'seedPhrase' && (
-        <SeedPhrase
+      {isOpen && (
+        <ImportKeyModal
           closeModal={closeModal}
-          navigate={navigate}
           isOpen={isOpen}
           accountId={accountId}
-          keyList={keyList}
-        />
-      )}
-      {modalStep === 'privateKey' && (
-        <PrivateKey
-          closeModal={closeModal}
           navigate={navigate}
-          isOpen={isOpen}
-          accountId={accountId}
-          keyList={keyList}
         />
       )}
     </>
