91bb3f2908454a83cb25305883e8124794dd7f39
eclipseeer <eclipseeer@gmail.com>
2024-03-01 14:01:03 +0200

    Fix getKeys and Signer Key bug

--- src/store/slices/keys/effects/loadKeysOnce.ts
+++ src/store/slices/keys/effects/loadKeysOnce.ts
@@ -1,15 +1,20 @@
 import { effect } from '../../../../react-vault';
 
-export const getKeys = effect(async ({ store }: any) => {
+export const loadKeysOnce = effect(async ({ slice, store }: any) => {
+  const isContractsLoadedToState = slice.getState((slice: any) => slice.isContractsLoadedToState);
+  if (isContractsLoadedToState) return;
+
   const [idb] = store.getEntities((store: any) => store.idb);
   const { networkId, spaceId } = store.getState((slice: any) => slice.networks.current);
+  const setKeysOnce = store.getActions((slice: any) => slice.keys.setKeysOnce);
 
   try {
-    return await idb.getAllFromIndex(
+    const keys = await idb.getAllFromIndex(
       'keys',
       'spaceId_networkId_importedAt',
       IDBKeyRange.bound([spaceId, networkId, -Infinity], [spaceId, networkId, Infinity]),
     );
+    setKeysOnce(keys);
   } catch (e) {
     console.log(e);
   }
