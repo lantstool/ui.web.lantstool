7f7e073fef8ad132ed495abf1fa5db00280a92a2
Boniface <sollomon35@gmail.com>
2024-03-07 19:47:51 +0200

    refactor v2

--- src/ui/components/Main/Contracts/Empty/Empty.tsx
+++ src/ui/components/Main/Contracts/Empty/Empty.tsx
@@ -1,10 +1,8 @@
 import cn from './Empty.module.css';
 import { CreateContract } from '../general/CreateContract/CreateContract.tsx';
 
-export const Empty = () => {
-  return (
-    <div className={cn.empty}>
-      <CreateContract styles={cn.modalContainer} />
-    </div>
-  );
-};
+export const Empty = () => (
+  <div className={cn.empty}>
+    <CreateContract styles={cn.modalContainer} />
+  </div>
+);
