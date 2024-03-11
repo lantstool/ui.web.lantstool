d35c266b5140c90da4ee09bce3e0772c61931f4d
Boniface <sollomon35@gmail.com>
2024-03-11 20:16:55 +0200

    refactor

--- src/ui/components/Main/Accounts/Account/Details/Item/Item.tsx
+++ src/ui/components/Main/Accounts/Account/Details/Item/Item.tsx
@@ -1,14 +1,12 @@
 import cn from './Item.module.css';
 import { CopyButton } from '../../../../general/CopyButton/CopyButton.tsx';
 
-export const Item = ({ title, data, copy = null }) => {
-  return (
-    <div className={cn.item}>
-      <p className={cn.title}>{title}</p>
-      <div className={cn.wrapper}>
-        <p className={cn.data}>{data}</p>
-        {copy && <CopyButton text={data} />}
-      </div>
+export const Item = ({ title, data, copy = null }) => (
+  <div className={cn.item}>
+    <p className={cn.title}>{title}</p>
+    <div className={cn.wrapper}>
+      <p className={cn.data}>{data}</p>
+      {copy && <CopyButton text={data} />}
     </div>
-  );
-};
+  </div>
+);
