22251eb92787a591f8e7b63aea3ad39ec615b027
Boniface <sollomon35@gmail.com>
2024-04-03 16:44:48 +0300

    fix duplicate

--- src/store/slices/transactions/actions/duplicateTransaction.ts
+++ src/store/slices/transactions/actions/duplicateTransaction.ts
@@ -1,8 +1,15 @@
 import { action } from '../../../../react-vault';
 
 export const duplicateTransaction = action(({ slice, payload: duplicate }: any) => {
+  const results = {
+    currentResult: null,
+    isOpen: false,
+    isLoading: false,
+    records: [],
+  };
+
   slice.list.splice(duplicate.order, 0, duplicate.transactionId);
-  slice.map[duplicate.transactionId] = duplicate;
+  slice.map[duplicate.transactionId] = { ...duplicate, results };
   slice.list.forEach((transactionId: any, index: number) => {
     const transaction = slice.map[transactionId];
     transaction.order = index;
