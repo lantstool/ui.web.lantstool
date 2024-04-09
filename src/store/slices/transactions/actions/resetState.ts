aeaa672a4c0849cf81bdfb4d009d034ee8e0d286
Taras <78810762+Bonifface@users.noreply.github.com>
2024-04-09 16:28:30 +0300

    refactor

--- src/store/slices/transactions/actions/resetState.ts
+++ src/store/slices/transactions/actions/resetState.ts
@@ -1,7 +1,6 @@
 import { action } from '../../../../react-vault';
 
 export const resetState = action(({ slice }: any) => {
-  console.log('resetTransactions');
   slice.list = [];
   slice.map = {};
   slice.isTransactionsLoadedToState = false;
