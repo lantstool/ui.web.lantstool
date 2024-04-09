3f4aebf50a01f787092aab5a084194b855506672
Taras <78810762+Bonifface@users.noreply.github.com>
2024-04-09 12:51:24 +0300

    result and validation

--- src/store/slices/calls/actions/setCalls.ts
+++ src/store/slices/calls/actions/setCalls.ts
@@ -3,10 +3,16 @@ import { action } from '../../../../react-vault';
 export const setCalls = action(({ slice, payload: calls }: any) => {
   const ids = [];
   const records: any = {};
+  const results = {
+    currentResult: null,
+    isOpen: false,
+    isLoading: false,
+    records: [],
+  };
 
   calls.forEach((call: any) => {
     ids.push(call.callId);
-    records[call.callId] = call;
+    records[call.callId] = { ...call, results };
   });
 
   slice.ids = ids;
