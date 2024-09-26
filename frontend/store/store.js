614de455edc38776dab246513d9fdfa4a3de960f
eclipseeer <eclipseeer@gmail.com>
2024-09-26 10:37:53 +0300

    Remove idb (IndexedDB)

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,14 +1,12 @@
 import { createStore } from '../../react-vault/index.js';
 import { initApp } from './effects/initApp.js';
 import { getAccessKeyList } from './effects/getAccessKeyList';
-import { idb } from './entities/idb_deprecated/idb';
 import { backend } from './entities/backend.js';
 import { spaces } from './slices/spaces/spaces.js';
 import { nearProtocol } from './slices/nearProtocol/nearProtocol.js';
 
 export const store = createStore({
   // entities
-  idb,
   backend,
   // effects
   initApp,
