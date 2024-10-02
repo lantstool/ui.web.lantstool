7e333b6ecfffc6fc42fe83b8347c67b903704e6b
Bonifocius <sollomon35@gmail.com>
2024-10-02 14:58:03 +0300

    LT 33 add icons and variables with mixins

---
+++ frontend/ui/assets/components/DeployContractLinear.jsx
@@ -0,0 +1,31 @@
+export const DeployContractLinear = ({ style }) => (
+  <svg
+    className={style}
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clipPath="url(#clip0_49_27532)">
+      <path
+        className={style}
+        d="M11.3333 6.00133C12.7833 6.00933 13.5686 6.074 14.0806 6.586C14.6666 7.172 14.6666 8.11466 14.6666 10V10.6667C14.6666 12.5527 14.6666 13.4953 14.0806 14.0813C13.4953 14.6667 12.5519 14.6667 10.6666 14.6667H5.33325C3.44792 14.6667 2.50459 14.6667 1.91925 14.0813C1.33325 13.4947 1.33325 12.5527 1.33325 10.6667V10C1.33325 8.11466 1.33325 7.172 1.91925 6.586C2.43125 6.074 3.21659 6.00933 4.66659 6.00133"
+        stroke="#868E96"
+        strokeLinecap="round"
+      />
+      <path
+        className={style}
+        d="M8 10V1.33333M8 1.33333L10 3.66667M8 1.33333L6 3.66667"
+        stroke="#868E96"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_49_27532">
+        <rect className={style} width="16" height="16" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+);
---
+++ frontend/ui/assets/components/InfoCircleLinear.jsx
@@ -0,0 +1,29 @@
+export const InfoCircleLinear = ({ style }) => (
+  <svg
+    className={style}
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clipPath="url(#clip0_49_27520)">
+      <path
+        className={style}
+        d="M7.99992 14.6667C11.6818 14.6667 14.6666 11.6819 14.6666 8C14.6666 4.3181 11.6818 1.33333 7.99992 1.33333C4.31802 1.33333 1.33325 4.3181 1.33325 8C1.33325 11.6819 4.31802 14.6667 7.99992 14.6667Z"
+        stroke="#868E96"
+      />
+      <path className={style} d="M8 11.3333V7.33333" stroke="#868E96" strokeLinecap="round" />
+      <path
+        className={style}
+        d="M7.99992 4.66667C8.36811 4.66667 8.66659 4.96514 8.66659 5.33333C8.66659 5.70152 8.36811 6 7.99992 6C7.63173 6 7.33325 5.70152 7.33325 5.33333C7.33325 4.96514 7.63173 4.66667 7.99992 4.66667Z"
+        fill="#868E96"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_49_27520">
+        <rect className={style} width="16" height="16" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+);
