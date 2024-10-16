62f563e4878357d10a26d2401cbf0f047a6457e2
eclipseeer <eclipseeer@gmail.com>
2024-10-16 15:42:24 +0300

    Rework reorder calls / fix import web worker

--- backend/db/requests/nearProtocol/calls/queries/getUpdateOrderQuery.js
+++ backend/db/requests/nearProtocol/calls/queries/getUpdateOrderQuery.js
@@ -1,29 +1,29 @@
 /*
   getUpdateOrderQuery generates a query like:
 
-  UPDATE near_protocol_transactions
+  UPDATE near_protocol_calls
   SET "order" = CASE
-    WHEN transactionId = 'tx1' THEN 1
-    WHEN transactionId = 'tx2' THEN 2
-    WHEN transactionId = 'tx3' THEN 3
+    WHEN callId = 'call1' THEN 1
+    WHEN callId = 'call2' THEN 2
+    WHEN callId = 'call3' THEN 3
     ELSE "order"
   END
-  WHERE transactionId IN ('tx1', 'tx2', 'tx3');
+  WHERE callId IN ('tx1', 'tx2', 'tx3');
 
-  We use it for update transactions order during reorder / remove tx operations
+  We use it for update calls order during reorder / remove call operations
 */
 
 export const getUpdateOrderQuery = (list) => {
   if (!list) throw new Error('Can`t reorder empty list');
 
-  const part1 = `UPDATE near_protocol_transactions SET "order" = CASE`;
+  const part1 = `UPDATE near_protocol_calls SET "order" = CASE`;
 
   const part2 = list
-    .map(({ transactionId, order }) => ` WHEN transactionId = '${transactionId}' THEN ${order}`)
+    .map(({ callId, order }) => ` WHEN callId = '${callId}' THEN ${order}`)
     .join('');
 
-  const part3 = `ELSE "order" END WHERE transactionId IN`;
-  const part4 = `(${list.map(({ transactionId }) => `'${transactionId}'`).join(', ')})`;
+  const part3 = `ELSE "order" END WHERE callId IN`;
+  const part4 = `(${list.map(({ callId }) => `'${callId}'`).join(', ')})`;
 
   return `${part1}${part2} ${part3} ${part4}`;
 };
