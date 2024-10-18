5a06409ab18c6fcc5463ef3e86f969070ffa7a83
Bonifocius <sollomon35@gmail.com>
2024-10-18 15:19:45 +0300

    Rework list

--- frontend/ui/components/_general/CopyButton/CopyButton.jsx
+++ frontend/ui/components/_general/CopyButton/CopyButton.jsx
@@ -1,12 +1,26 @@
-import cn from './CopyButton.module.scss';
 import { CopyOutline } from '../icons/CopyOutline.jsx';
+import { CheckCircleOutline } from '../icons/CheckCircleOutline.jsx';
+import { useState } from 'react';
+import cn from './CopyButton.module.scss';
 
-const copyText = (copy) => {
-  navigator.clipboard.writeText(copy);
+export const CopyButton = ({ color = 'primary', copy, disabled = false }) => {
+  const [copied, setCopied] = useState(false);
+
+  const copyTextToClipboard = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    navigator.clipboard.writeText(copy);
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+  };
+
+  const iconColor = color === 'primary' ? cn.icon : cn.iconSecondary;
+
+  return (
+    <button disabled={disabled} type="button" onClick={copyTextToClipboard} className={cn.button}>
+      {copied ? <CheckCircleOutline style={iconColor} /> : <CopyOutline style={iconColor} />}
+    </button>
+  );
 };
-
-export const CopyButton = ({ copy, disabled = false }) => (
-  <button disabled={disabled} type="button" onClick={() => copyText(copy)} className={cn.button}>
-    <CopyOutline style={cn.icon} />
-  </button>
-);
