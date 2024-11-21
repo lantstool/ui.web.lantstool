ca798bd72fe73f714bc184704862e2d14ed33c0f
Bonifocius <sollomon35@gmail.com>
2024-11-21 13:35:44 +0200

    Add settings design

--- frontend/ui/components/Settings/Settings.jsx
+++ frontend/ui/components/Settings/Settings.jsx
@@ -1,5 +1,9 @@
 import { useStoreEffect } from '@react-vault';
 import { useNavigate } from 'react-router-dom';
+import { Button } from '../_general/Button/Button.jsx';
+import { ExportLinear } from '../_general/icons/ExportLinear.jsx';
+import {Label} from '../_general/Label/Label.jsx';
+import cn from './Settings.module.scss';
 
 export const Settings = () => {
   const resetApp = useStoreEffect((store) => store.resetApp);
@@ -12,12 +16,52 @@ export const Settings = () => {
   const clearHistory = () => resetHistory({ navigate });
 
   return (
-    <div>
-      <h1> General Setting</h1>
-      <button onClick={goBack}>Back</button>
-      <button onClick={createBackup}>Create Backup</button>
-      <button onClick={clearApp}>Reset App</button>
-      <button onClick={clearHistory}>Reset History</button>
-    </div>
+    <>
+      <button className={cn.backBtn} onClick={goBack}>Back</button>
+      <Label color='green'>
+        My color
+      </Label>
+      <div className={cn.settings}>
+        <div className={cn.content}>
+          <h1 className={cn.title}>Setting</h1>
+          <div className={cn.container}>
+            <div className={cn.wrapper}>
+              <p className={cn.subtitle}>Create backup file</p>
+              <p className={cn.subtitleLight}>
+                This will export all data including existing spaces with their content. Then you
+                will be able to restore your content from this file.
+              </p>
+            </div>
+            <Button color="secondary" size="medium" IconLeft={ExportLinear} onClick={createBackup}>
+              Create Backup
+            </Button>
+          </div>
+          <div className={cn.container}>
+            <div className={cn.wrapper}>
+              <p className={cn.subtitle}>Reset the app</p>
+              <p className={cn.subtitleLight}>
+                If you encounter technical issues or want to erase all content, you can reset it,
+                but all data will be lost forever.
+              </p>
+            </div>
+            <Button color="danger" size="medium" onClick={clearApp}>
+              Reset App
+            </Button>
+          </div>
+          <div className={cn.container}>
+            <div className={cn.wrapper}>
+              <p className={cn.subtitle}>Reset the app</p>
+              <p className={cn.subtitleLight}>
+                If you encounter technical issues or want to erase all content, you can reset it,
+                but all data will be lost forever.
+              </p>
+            </div>
+            <Button color="danger" size="medium" onClick={clearHistory}>
+              Reset History
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
