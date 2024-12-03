867ef6752579c0a321bf724757bad6bfb5fd32f5
eclipseeer <eclipseeer@gmail.com>
2024-12-03 15:04:43 +0200

    Add user-defined rpc card

--- frontend/ui/components/Space/NearProtocol/Network/Settings/RpcNodes/Group/UserCreatedRpc/UserCreatedRpc.jsx
+++ frontend/ui/components/Space/NearProtocol/Network/Settings/RpcNodes/Group/UserCreatedRpc/UserCreatedRpc.jsx
@@ -1,5 +1,36 @@
+import { Button } from '../../../../../../../_general/Button/Button.jsx';
+import { SelectButton } from '../_general/SelectButton/SelectButton.jsx';
+import defaultRpcLogo from '@assets/logos/default-rpc-logo.png';
 import cn from './UserCreatedRpc.module.scss';
 
-export const UserCreatedRpc = () => {
-  return <div>UserCreatedRpc</div>
-}
+export const UserCreatedRpc = ({
+  rpc,
+  groupActiveRpc,
+  rpcType,
+  spaceId,
+  networkId,
+  updateActiveRpc,
+}) => {
+  const isSelected = groupActiveRpc.rpc?.id === rpc.id;
+
+  const select = () => {
+    if (isSelected) return;
+    updateActiveRpc({ spaceId, networkId, rpcType, autoBalance: false, rpc });
+  };
+
+  return (
+    <div className={cn.rpc}>
+      <div className={cn.selectButtonWrapper}>
+        <SelectButton isSelected={isSelected} select={select} />
+      </div>
+      <img src={defaultRpcLogo} alt={`${rpc.name} logo`} className={cn.logo} />
+      <div className={cn.nameUrl}>
+        <h3 className={cn.name}>{rpc.name}</h3>
+        <span className={cn.url}>{rpc.url}</span>
+      </div>
+      <div className={cn.menuButtonWrapper}>
+        <Button size="small" color="tertiary" IconLeft={() => <span className={cn.menuIcon} />} />
+      </div>
+    </div>
+  );
+};
