c4d0740f82a76b06ba4db9e6d6b6eabd8d042054
eclipseeer <eclipseeer@gmail.com>
2024-12-23 10:58:25 +0200

    Fix sidebar icons

--- frontend/ui/components/Space/NearProtocol/Network/Sidebar/Navigation/LinkItem/LinkItem.jsx
+++ frontend/ui/components/Space/NearProtocol/Network/Sidebar/Navigation/LinkItem/LinkItem.jsx
@@ -1,10 +1,13 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useMatch, useResolvedPath } from 'react-router-dom';
+import cnm from 'classnames';
 import cn from './LinkItem.module.scss';
 
-export const LinkItem = ({ name, Icon, to, isSidebarMinimized }) => {
+export const LinkItem = ({ name, icon, activeIcon, to, isSidebarMinimized }) => {
+  const resolved = useResolvedPath(to);
+  const match = useMatch({ path: resolved.pathname, end: false });
   return (
-    <NavLink className={({ isActive }) => (isActive ? cn.active : cn.container)} to={to}>
-      <Icon style={cn.icon} />
+    <NavLink className={match ? cn.active : cn.container} to={to}>
+      <span className={cnm(cn.iconContainer, match ? activeIcon : icon)} />
       {!isSidebarMinimized && <h4 className={cn.title}>{name}</h4>}
     </NavLink>
   );
