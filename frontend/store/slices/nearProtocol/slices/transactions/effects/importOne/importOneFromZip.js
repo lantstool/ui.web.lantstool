eca67c9c47b2a227a2e9d64e7f488d0401e8142b
eclipseeer <eclipseeer@gmail.com>
2025-01-09 22:21:01 +0200

    Add import tx from zip feature, fix duplicate tx

--- frontend/store/slices/nearProtocol/slices/transactions/effects/importOne/importOneFromZip.js
+++ frontend/store/slices/nearProtocol/slices/transactions/effects/importOne/importOneFromZip.js
@@ -1,34 +1,12 @@
 import { effect } from '@react-vault';
-import { unzipSync, strFromU8 } from 'fflate';
-
-const unzipJson = async (file, setError) => {
-  try {
-    const arrayBuffer = await file.arrayBuffer();
-    const files = unzipSync(new Uint8Array(arrayBuffer));
-    // return data (second param) of the first file (there are always only 1 file)
-    const json = strFromU8(Object.entries(files)[0][1]);
-    return JSON.parse(json);
-  } catch (e) {
-    console.log(e);
-    setError('file', { type: 'unzipJson', message: "Can't extract JSON from the file" });
-  }
-};
-
-const validateJson = async (json, setError, yupSchema) => {
-  try {
-    await yupSchema.validate({ json });
-    return true;
-  } catch (e) {
-    setError('file', { type: 'invalidJson', message: e.message });
-    return false;
-  }
-};
+import { unzipJsonImportFile } from '../../../../helpers/unzipJsonImportFile.js';
+import { validateJson } from '../../../../helpers/validateJson.js';
 
 export const importOneFromZip = effect(async ({ slice, payload }) => {
   const { spaceId, networkId, formValues, navigate, closeModal, setError, yupSchema } = payload;
   const importOneFromJson = slice.getEffects((slice) => slice.importOneFromJson);
 
-  const json = await unzipJson(formValues.file, setError);
+  const json = await unzipJsonImportFile(formValues.file, setError);
   if (!json) return;
 
   const isValid = await validateJson(json, setError, yupSchema);
