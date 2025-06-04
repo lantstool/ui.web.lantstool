6442f4134754d447cb23e43ec72849a5f23a0a8d
Bonifocius <sollomon35@gmail.com>
2025-06-04 17:01:35 +0300

    LT-211

--- frontend/ui/components/Space/NearProtocol/Network/Accounts/Account/AccountKeys/Items/Items.jsx
+++ frontend/ui/components/Space/NearProtocol/Network/Accounts/Account/AccountKeys/Items/Items.jsx
@@ -1,16 +1,29 @@
-import { CopyButton } from '../../../../../../../_general/CopyButton/CopyButton.jsx';
+import { CopyButton } from '@gc/CopyButton/CopyButton.jsx';
 import { FunctionCall } from './FunctionCall/FunctionCall.jsx';
-import { Tooltip } from '../../../../../../../_general/Tooltip/Tooltip.jsx';
+import { Tooltip } from '@gc/Tooltip/Tooltip.jsx';
+import { useNavigate, useParams } from 'react-router-dom';
 import cn from './Items.module.scss';
 
 export const Items = ({ keys, type, name }) => {
   if (keys.length === 0) return;
+  const navigate = useNavigate();
+  const { spaceId, networkId } = useParams();
+
+  const redirectExistKeys = (key) => {
+    if (key.isLocalExists) {
+      navigate(`/space/${spaceId}/near-protocol/${networkId}/keys/${key.publicKey}`);
+    }
+  };
 
   return (
     <div className={cn.container}>
       <h2 className={cn.title}>{name}</h2>
       {keys.map((key) => (
-        <div key={key.publicKey} className={cn.item}>
+        <div
+          key={key.publicKey}
+          className={key.isLocalExists ? cn.itemPointer : cn.item}
+          onClick={() => redirectExistKeys(key)}
+        >
           <div className={cn.wrapper}>
             <p className={cn.subtitle}>{key.publicKey}</p>
             <div className={cn.buttonWrapper}>
